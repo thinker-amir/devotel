3b9a0b14c9f31bbd09947a76205a0a1c8b32e7b0
thinker <thinker.amir@gmail.com>
2025-07-17 21:08:56 +0330

    Add TypeORM to connect to the postgres database

--- nestjs/src/app.module.ts
+++ nestjs/src/app.module.ts
@@ -1,5 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import typeormConfig from './database/config/typeorm.config';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [typeormConfig],
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) =>
+        configService.get('typeormConfig') as TypeOrmModuleOptions,
+    }),
+  ],
 })
 export class AppModule {}
---
+++ nestjs/src/database/config/typeorm.config.ts
@@ -0,0 +1,50 @@
+import { registerAs } from '@nestjs/config';
+import { config as dotenvConfig } from 'dotenv';
+import { DataSource, DataSourceOptions } from 'typeorm';
+
+const environmentName = process.env.NODE_ENV || 'development';
+console.info(`The environment is: ${environmentName}`);
+
+dotenvConfig({ quiet: true });
+
+const config = {
+  type: 'postgres',
+  host: `${process.env.DATABASE_HOST}`,
+  port: `${process.env.DATABASE_PORT}`,
+  username: `${process.env.DATABASE_USERNAME}`,
+  password: `${process.env.DATABASE_PASSWORD}`,
+  database: `${process.env.DATABASE_NAME}`,
+  autoLoadEntities: true,
+  synchronize: false,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  migrations: ['dist/database/migrations/*{.ts,.js}'],
+};
+
+const configEnvMap = new Map<string, Partial<DataSourceOptions>>([
+  [
+    'development',
+    {
+      synchronize: false,
+    },
+  ],
+  [
+    'testing',
+    {
+      host: `${process.env.TEST_DATABASE_HOST}`,
+      password: `${process.env.TEST_DATABASE_PASSWORD}`,
+      synchronize: true,
+      dropSchema: true,
+    },
+  ],
+]);
+
+function getConfig(): DataSourceOptions {
+  const envConfig = configEnvMap.get(environmentName);
+  if (envConfig === undefined) {
+    throw new Error(`Unexpected value [${environmentName}]`);
+  }
+  return { ...config, ...envConfig } as DataSourceOptions;
+}
+
+export default registerAs('typeormConfig', () => getConfig());
+export const connectionSource = new DataSource(config as DataSourceOptions);
